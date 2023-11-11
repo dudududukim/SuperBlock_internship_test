cc34788b624f3848beddc012975f7ee22a02e1ea
dudududukim <kdhluck@gmail.com>
2023-11-11 17:18:36 +0900

    game intro

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import BallonGame from './BallonGame';
+
+function App() {
+  return (
+    <BallonGame />
+  );
+}
+
+export default App;
---
+++ src/BallonGame.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class BallonGame extends Component {
+    render() {
+        return (
+            <div>
+               BallonGame 
+            </div>
+        );
+    }
+}
+
+export default BallonGame;
